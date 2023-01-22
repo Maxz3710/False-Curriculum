99a7db8a920157ee3d5e1326ca8c0881c41cc5b0
Max Longoni <maximxz07@gmail.com>
2023-01-22 20:53:27 -0300

    Proyecto final - Programar 4.0 - Curriculum finalizado

---
+++ js/curriculum.js
@@ -0,0 +1,32 @@
+
+document.getElementById('mostrarDatosPersonales').addEventListener('click', function() {
+    document.getElementById('contenidoDatosPersonales').style.display = 'block';
+});
+
+document.getElementById('ocultarDatosPersonales').addEventListener('click', function() {
+    document.getElementById('contenidoDatosPersonales').style.display = 'none';
+});
+
+document.getElementById('mostrarDatosDeEstudio').addEventListener('click', function() {
+    document.getElementById('contenidoDatosDeEstudio').style.display = 'block';
+});
+
+document.getElementById('ocultarDatosDeEstudio').addEventListener('click', function() {
+    document.getElementById('contenidoDatosDeEstudio').style.display = 'none';
+});
+
+document.getElementById('mostrarDatosDeTrabajo').addEventListener('click', function() {
+    document.getElementById('contenidoDatosDeTrabajo').style.display = 'block';
+});
+
+document.getElementById('ocultarDatosDeTrabajo').addEventListener('click', function() {
+    document.getElementById('contenidoDatosDeTrabajo').style.display = 'none';
+});
+
+document.getElementById('mostrarMasInfo').addEventListener('click', function() {
+    document.getElementById('contenidoMasInfo').style.display = 'block';
+});
+
+document.getElementById('ocultarMasInfo').addEventListener('click', function() {
+    document.getElementById('contenidoMasInfo').style.display = 'none';
+});
